4a5fb7f4e46659ee54fa83c4e9613a6024e8c12f
kulakoff <anton.kulakoff@gmail.com>
2022-02-02 01:57:49 +0300

    fixing temporary changes per day

---
+++ src/components/repositoryList/RepositoryList.tsx
@@ -0,0 +1,41 @@
+import React, { FC } from "react";
+import classes from "./RepositoryList.module.css"
+import searchButton from "./../../assets/img/searchButton.svg"
+
+
+const RepositoryList: FC = () => {
+  return (
+    <div className={classes.container}>
+      <div className={classes.searchBar}>
+        <input type="text"
+          className={classes.searchBar__input}
+          placeholder="Введите название организации" />
+        <button className={classes.searchBar__button}></button>
+      </div>
+      <div className={classes.reposList}>
+        <div className={classes.item}>
+          <div className={classes.item__avatar}>
+            <div className={classes.item__img}></div>
+            <div className={classes.item__defaltBackground}>
+              <span className={classes.item__repoFirsLetter}>K</span>
+            </div>
+
+          </div>
+          <div className={classes.item__content}>
+            <span className={classes.item__repoName}>kts-school-frontend</span>
+            <span className={classes.item__orgName}>ktsstudio</span>
+            <div className={classes.item__info}>
+              <div className={classes.item__stars}>
+                <div className={classes.item__starImg}></div>
+                <span className={classes.item__starsCount}>123</span>
+              </div>
+              <span className={classes.item__updated}>Updated 21 Jul</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
+}
+export default RepositoryList
