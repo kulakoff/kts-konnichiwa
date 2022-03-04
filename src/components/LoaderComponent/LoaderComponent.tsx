10ab58a8a9979f0f1740bc92fb69191e9d1c092b
kulakoff <anton.kulakoff@gmail.com>
2022-03-05 01:49:48 +0300

    modify css to scss modules

--- src/components/LoaderComponent/LoaderComponent.tsx
+++ src/components/LoaderComponent/LoaderComponent.tsx
@@ -1,5 +1,5 @@
 import loader from "@assets/img/spinner3.svg"
-import styles from "./LoaderComponent.module.css"
+import styles from "./LoaderComponent.module.scss"
 
 const LoaderComponent =()=>{
   return <div className={styles.loader}><img src={loader} alt="" /></div>
